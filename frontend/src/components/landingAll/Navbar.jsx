94f62c8ec2cd9081f9e5e0db7dbf4cdf30604e5d
JiachenWu-NEU <wu.jiachen1@northeaster.edu>
2025-12-11 14:39:34 -0500

    Unifying the name of the website

--- frontend/src/components/landingAll/Navbar.jsx
+++ frontend/src/components/landingAll/Navbar.jsx
@@ -1,12 +1,14 @@
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
+import { APP_NAME } from "../../config/appConfig";
+
 export default function Navbar() {
   return (
     <AppBar position="static" color="primary">
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         <Typography variant="h6" sx={{ fontWeight: 600 }}>
-          CampusTrade
+          {APP_NAME}
         </Typography>
         <div>
           <Button color="inherit" component={Link} to="/login">Login</Button>
